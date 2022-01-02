7691a6af52154d62100e86971e1a1a7a9cf1a3f2
roshan-rks28 <you@example.com>
2022-01-02 18:47:33 +0530

    Version1

---
+++ functions.js
@@ -0,0 +1,11 @@
+function handleData(){
+	var value = document.querySelector("#name").value;
+	var num = document.querySelector("#num").value;
+	var email = document.querySelector("#email").value;
+	var gen = document.querySelector("#gen").value;
+	var course = document.querySelector("#course").value;
+	var alumini = document.querySelector("#alumini").value;	
+	console.log(value);
+	alert(alumini);
+
+}
